f06e4a3631de4ac33de1fb0b8dd06689690fa9d8
Gian Luca Oliveira <gloliveira539@gmail.com>
2022-07-18 20:28:47 -0300

    1) keyboard event - 2) Text display on gamepage

--- gamepage/JSFiles/game.js
+++ gamepage/JSFiles/game.js
@@ -10,60 +10,109 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 import { Choice } from "./choice.js";
 import { Statement } from "./statement.js";
 let choiceBtns;
-let textWindow;
+let scriptWindow;
+let scriptTags;
 let textObjects;
 let index;
-function init() {
+let start;
+let block;
+function loadElements() {
     choiceBtns = document.getElementsByClassName("choice-btn");
-    textWindow = document.getElementsByClassName("text-window");
+    scriptWindow = document.querySelector(".script-window");
+    scriptTags = ["[empty line]", "[?]", "[Choice 1]", "[Choice 2]", "[C1]", "[C2]", "*", "**"];
+    start = false;
+    block = false;
     textObjects = [];
     index = 0;
     loadTextObjects();
-    sendTextToFront(index);
+    keyboardSupport();
 }
-function sendTextToFront(index) {
+function keyboardSupport() {
+    document.getElementsByTagName("body")[0].addEventListener('keyup', e => {
+        if (e.code == "Space") {
+            pressed();
+        }
+        console.log(e);
+    });
 }
-function showNextLine() {
+function pressed() {
+    if (!start) {
+        hideText();
+        start = true;
+        pressed();
+    }
+    else if (block == false) {
+        let line = textObjects.filter(obj => { return obj.getId() === index; })[0];
+        if (line.getType() == "Choice") {
+            setChoices(line);
+        }
+        else {
+            sendTextToFront(line);
+            index += 1;
+        }
+    }
 }
-function showOptions() {
+function sendTextToFront(line) {
+    const newElement = document.createElement("p");
+    newElement.className = 'script-line';
+    const newText = document.createTextNode(line.getText());
+    newElement.appendChild(newText);
+    scriptWindow.appendChild(newElement);
+}
+function hideText() {
+    document.getElementsByClassName("start-text")[0].setAttribute("style", "display: none;");
+}
+function setChoices(line) {
+    choiceBtns[0].innerHTML = line.getChoice(0).getText();
+    choiceBtns[1].innerHTML = line.getChoice(1).getText();
+    block = true;
 }
 function selectOption() {
+}
+function removeTags(text) {
+    let tagless = "";
+    scriptTags.forEach((tag) => {
+        if (text.includes(tag)) {
+            tagless = text.replace(tag, "");
+        }
+    });
+    return tagless;
 }
 function loadTextObjects() {
     return __awaiter(this, void 0, void 0, function* () {
         const textArray = yield readFromFile();
         let templateChoice;
         textArray.forEach((text, id) => {
+            let taglessText = removeTags(text);
             if (text.includes("[empty line]")) {
-                textObjects.push(new Statement(id, " ", "Empty"));
+                textObjects.push(new Statement(id, taglessText, "Empty"));
             }
             else if (text.includes("[?]")) {
-                templateChoice = new Statement(id, text, "Question");
+                templateChoice = new Statement(id, taglessText, "Question");
             }
             else if (text.includes("[Choice 1]") || text.includes("[Choice 2]")) {
-                templateChoice.addChoice(new Choice(id, text));
+                templateChoice.addChoice(new Choice(id, taglessText));
             }
             else if (text.includes("[C1]")) {
-                templateChoice.getChoice(0).addEvent(id, text, "Consquence 1");
+                templateChoice.getChoice(0).addEvent(id, taglessText, "Consquence 1");
             }
             else if (text.includes("[C2]")) {
-                templateChoice.getChoice(1).addEvent(id, text, "Consquence 2");
+                templateChoice.getChoice(1).addEvent(id, taglessText, "Consquence 2");
             }
             else if (text.includes("[end of Choice 2]")) {
                 textObjects.push(templateChoice);
             }
             else {
                 let type = "";
-                if (text.includes("*")) {
-                    type = "Narration";
-                }
-                else if (text.includes("**")) {
+                if (text.includes("**")) {
                     type = "Talk";
                 }
-                textObjects.push(new Statement(id, text, type));
+                else if (text.includes("*")) {
+                    type = "Narration";
+                }
+                textObjects.push(new Statement(id, taglessText, type));
             }
         });
-        console.log(textObjects);
     });
 }
 function readFromFile() {
@@ -76,4 +125,4 @@ function readFromFile() {
         return text.split(/\r?\n/);
     });
 }
-init();
+loadElements();
--- gamepage/TSFiles/game.ts
+++ gamepage/TSFiles/game.ts
@@ -2,33 +2,88 @@ import { Choice } from "./choice.js";
 import { Statement } from "./statement.js";
 
 let choiceBtns: HTMLCollectionOf<Element>;
-let textWindow: HTMLCollectionOf<Element>;
+let scriptWindow: Element;
+let scriptTags: string[];
 let textObjects: Statement[];
 let index: number;
+let start: boolean;
+let block: boolean;
 
-function init() {
-    choiceBtns = document.getElementsByClassName("choice-btn")
-    textWindow = document.getElementsByClassName("text-window")
+function loadElements() {
+    choiceBtns = document.getElementsByClassName("choice-btn");
+    scriptWindow = document.querySelector(".script-window") as Element;
+    scriptTags = ["[empty line]", "[?]", "[Choice 1]", "[Choice 2]", "[C1]", "[C2]", "*", "**"];
 
+    start = false;
+    block = false;
     textObjects = [];
     index = 0;
     loadTextObjects();
-    sendTextToFront(index);
-}
-
-function sendTextToFront(index: number) {
-}
-
-function showNextLine() {
+    keyboardSupport();
 
 }
 
-function showOptions() {
+function keyboardSupport() {
+    document.getElementsByTagName("body")[0].addEventListener('keyup', e => {
+        if (e.code == "Space") {
+            pressed();
+        }
+        console.log(e);
+    });
+}
 
+function pressed() {
+    if (!start) {
+        hideText();
+        start = true;
+        pressed();
+
+    } else if (block == false) {
+
+        let line: Statement | Choice = textObjects.filter(obj => { return obj.getId() === index })[0];
+        
+        if (line.getType() == "Choice") {
+            setChoices(line);
+        } else {
+            sendTextToFront(line);
+            index += 1;
+        }
+    }
+
+}
+
+function sendTextToFront(line: Statement | Choice) {
+    const newElement = document.createElement("p");
+    newElement.className = 'script-line';
+    const newText = document.createTextNode(line.getText());
+    newElement.appendChild(newText);
+    scriptWindow.appendChild(newElement);
+}
+
+function hideText() {
+    document.getElementsByClassName("start-text")[0].setAttribute("style", "display: none;")
+}
+
+function setChoices(line: Statement) {
+    choiceBtns[0].innerHTML = line.getChoice(0).getText();
+    choiceBtns[1].innerHTML = line.getChoice(1).getText();
+    block = true;
 }
 
 function selectOption() {
 
+}
+
+function removeTags(text: string) {
+    let tagless: string = "";
+
+    scriptTags.forEach((tag: string) => {
+        if (text.includes(tag)) {
+            tagless = text.replace(tag, "");
+        }
+    });
+
+    return tagless;
 }
 
 async function loadTextObjects() {
@@ -39,24 +94,26 @@ async function loadTextObjects() {
     textArray.forEach(
         (text, id) => {
 
+            let taglessText: string = removeTags(text);
+
             if (text.includes("[empty line]")) {
-                textObjects.push(new Statement(id, " ", "Empty"));
+                textObjects.push(new Statement(id, taglessText, "Empty"));
             }
 
             else if (text.includes("[?]")) {
-                templateChoice = new Statement(id, text, "Question");
+                templateChoice = new Statement(id, taglessText, "Question");
             }
 
             else if (text.includes("[Choice 1]") || text.includes("[Choice 2]")) {
-                templateChoice.addChoice(new Choice(id, text));
+                templateChoice.addChoice(new Choice(id, taglessText));
             }
 
             else if (text.includes("[C1]")) {
-                templateChoice.getChoice(0).addEvent(id, text, "Consquence 1");
+                templateChoice.getChoice(0).addEvent(id, taglessText, "Consquence 1");
             }
 
             else if (text.includes("[C2]")) {
-                templateChoice.getChoice(1).addEvent(id, text, "Consquence 2");
+                templateChoice.getChoice(1).addEvent(id, taglessText, "Consquence 2");
             }
 
             else if (text.includes("[end of Choice 2]")) {
@@ -65,18 +122,15 @@ async function loadTextObjects() {
 
             else {
                 let type: string = "";
-                if (text.includes("*")) {
-                    type = "Narration";
-                } else if (text.includes("**")) {
+                if (text.includes("**")) {
                     type = "Talk";
+                } else if (text.includes("*")) {
+                    type = "Narration";
                 }
-
-                textObjects.push(new Statement(id, text, type));
+                textObjects.push(new Statement(id, taglessText, type));
             }
         }
     )
-
-    console.log(textObjects);
 
 }
 
@@ -94,4 +148,4 @@ async function readFromFile() {
 
 }
 
-init();
+loadElements();
